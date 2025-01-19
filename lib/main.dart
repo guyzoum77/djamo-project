e8627dc9ebbf96b38e58809b5d3452d11776216e
guyzoum77 <guyzoum7700@gmail.com>
2025-01-19 23:14:41 +0000

    finishing work at 100% and deleting onboarding directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
-import 'package:djamo_test/features/onboarding/presentation/pages/onboarding_page.dart';
-import 'package:djamo_test/features/task/presentation/bloc/task_bloc.dart';
 import 'package:djamo_test/features/task/presentation/pages/task_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'features/task/presentation/pages/loaded/loaded_task_view.dart';
-import 'features/task/presentation/pages/task_view.dart';
 import 'injection_container.dart' as di;
-import 'injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
